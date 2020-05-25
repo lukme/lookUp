07287abfbc7611ea35746e7a21ce9d9cea03a5f1
lukme <lukme00@gmail.com>
2020-05-25 19:23:29 +0200

    added summaryPage

--- src/components/SummaryPage/SummaryPage.tsx
+++ src/components/SummaryPage/SummaryPage.tsx
@@ -1,4 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import { AiOutlineHome } from 'react-icons/ai';
+import { FaLuggageCart, FaRegCalendarAlt } from 'react-icons/fa';
+import { BsFillPersonFill } from 'react-icons/bs';
+import { GiConvergenceTarget } from 'react-icons/gi';
+
 import { FlightData } from '../MainPage/MainPage';
 
 interface Props {
@@ -6,18 +12,96 @@ interface Props {
 }
 
 const SummaryPage: React.FunctionComponent<Props> = (props: Props) => {
-    const { flightData } = props;
+    const { flightData } = props,
+        [passengers, setPassengers] = useState<number>();
 
-    console.log(flightData)
+    useEffect(() => {
+        !passengers && formatPassengers();
+    });
+
+    const formatPassengers = () => {
+        setPassengers(
+            parseInt(flightData.passengers.adults)
+            + parseInt(flightData.passengers.adults)
+            + parseInt(flightData.passengers.babies)
+        )
+    }
 
     return (
         <>
             <div className="summary">
                 <div className="summary__options">
-                    dupa
+                    <div className="summary__option">
+                        <h3 className="summary__subtitle--container">
+                            <AiOutlineHome size={20} />
+                            <h3 className="summary__subtitle">
+                                Origin
+                            </h3>
+                            <div className="underline" />
+                        </h3>
+                        <p className="summary__data">
+                            {flightData.origin}
+                        </p>
+                    </div>
+                    <div className="summary__option">
+                        <div className="summary__subtitle--container">
+                            <GiConvergenceTarget size={20} />
+                            <h3 className="summary__subtitle">
+                                Destination
+                            </h3>
+                            <div className="underline" />
+                        </div>
+                        <p className="summary__data">
+                            {flightData.destination}
+                        </p>
+                    </div>
+                    <div className="summary__option">
+                        <div className="summary__subtitle--container">
+                            <FaRegCalendarAlt size={20} />
+                            <h3 className="summary__subtitle">
+                                Departure
+                            </h3>
+                            <div className="underline" />
+                        </div>
+                        <p className="summary__data">
+                            {flightData.departure}
+                        </p>
+                    </div>
+                    <div className="summary__option">
+                        <div className="summary__subtitle--container">
+                            <BsFillPersonFill size={20} />
+                            <h3 className="summary__subtitle">
+                                Passengers
+                            </h3>
+                            <div className="underline" />
+                        </div>
+                        <p className="summary__data">
+                            {passengers}
+                        </p>
+                    </div>
+                    <div className="summary__option">
+                        <div className="summary__subtitle--container">
+                            <FaLuggageCart size={20} />
+                            <h3 className="summary__subtitle">
+                                Luggage
+                            </h3>
+                            <div className="underline" />
+                        </div>
+                        <p className="summary__data">
+                            {flightData.origin}
+                        </p>
+                    </div>
+                    <button className="button button--order">
+                        Order
+                    </button>
+                    <p className='summary__warning'>You have to be logged in to finalize your order</p>
                 </div>
                 <div className="summary__airplane">
-                    <img src="/airplane_wing.jpg" alt="wing"/>
+                    <TransformWrapper>
+                        <TransformComponent>
+                            {/* AIRPLANE PLACEHOLDER */}
+                        </TransformComponent>
+                    </TransformWrapper>
                 </div>
                 <div className="summary__options summary__options--aesthetical" />
             </div>
